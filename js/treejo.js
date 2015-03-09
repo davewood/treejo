4c6636d6c0abadbf6a17238541e2f83e88f53799
David Schmidt <mail@davidschmidt.at>
2015-03-09 15:07:32 +0100

    add scroll_to_node + highlight_panel functions

--- js/treejo.js
+++ js/treejo.js
@@ -1,6 +1,12 @@
 (function( treejo,  undefined ) {
     'use strict';
 
+        //var url = 'http://localhost:3000/get_node';
+    var options = {
+        "window_top_offset": 10,
+        "url": 'file:///home/david/dev/treejo/data.json'
+    };
+
     function get_node(title, body) {
         return '<div class="node">'
              +   '<div class="node-panel">'
@@ -13,13 +19,11 @@
              + '</div>';
     }
     function node_show(node) {
-        //var url = 'http://localhost:3000/get_node';
-        var url = 'file:///home/david/dev/treejo/data.json';
         var content = node.children('.node-content');
         if ( content.length === 0 ) {
             content = $('<div class="node-content" style="display:none;"></div>');
             $.ajax({
-                url:     url,
+                url:     options.url,
                 async:   false,
                 type:    'GET',
                 data:    {"node_id":"1"},
@@ -35,6 +39,44 @@
             node.append(content);
         }
         content.slideDown({ duration: 500 });
+    }
+    function node_hide(node) {
+        node.children('.node-content').slideUp({ duration: 500 });
+    }
+
+    function scroll_to_node(node) {
+        $('html, body').animate(
+            {
+                scrollTop: node.offset().top - options.window_top_offset
+            },
+            500
+        );
+        highlight_panel(node);
+    }
+
+    function highlight_panel(node) {
+        var panel = node.children('.node-panel');
+        panel.addClass('node-panel-highlight');
+        setTimeout(
+          function() {
+              panel.removeClass('node-panel-highlight');
+          },
+          2000
+        );
+    }
+    function is_visible(node) {
+        var view_top     = window.pageYOffset + options.window_top_offset;
+        var view_bottom  = view_top + $(window).height();
+        var panel        = node.children('.node-panel');
+        var panel_top    = panel.offset().top;
+        var panel_bottom = panel_top + panel.height();
+
+        if ( panel_top > view_top && panel_bottom < view_bottom ) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     treejo.init = function(selector) {
@@ -56,7 +98,7 @@
                     function(event) {
                         var node_toggle = $(event.target);
                         var node = node_toggle.closest('.node');
-                        node.children('.node-content').slideUp({ duration: 500 });
+                        node_hide(node);
                         node_toggle.toggleClass('node-closed node-open');
                     });
             tree.on('click',
@@ -64,8 +106,9 @@
                     function(event) {
                         var node = $(event.target).closest('.node');
                         var node_toggle = node.children('.node-panel').children('.node-panel-header').children('.node-toggle');
-                        node.children('.node-content').slideUp({ duration: 500 });
+                        node_hide(node);
                         node_toggle.toggleClass('node-closed node-open');
+                        if( !is_visible(node) ) { scroll_to_node(node); }
                     });
         }
     };
