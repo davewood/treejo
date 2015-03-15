74fefa1d1f0cdebe523a6e87ab77080431a9e5a7
David Schmidt <mail@davidschmidt.at>
2015-03-15 19:03:04 +0100

    markup for #treejo2

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     treejo.create(
         $('#mytreejo2'),
         {
-            'html_node_show_all': '<a title="open all child nodes" class="node-show-all">open all</a> ',
+            'html_node_show_all': '',
             'html_node_toggle':   '<a class="node-toggle">&gt;</a> ',
             'html_node_open':     'v',
             'html_node_closed':   '&gt;',
             'highlight_duration': 9999,
-            'slide_duration':     3000
+            'slide_duration':     1000
         }
     );
 });
